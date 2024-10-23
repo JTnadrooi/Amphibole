9aee835450a2561bce6b24ae9cb12ddf825f3dfe
MikeValpoort <6037098@mborijnland.nl>
2024-10-23 13:28:23 +0200

    prey to god

---
+++ js/canvas.js
@@ -0,0 +1,218 @@
+
+
+
+
+
+
+
+
+
+function load() { // No support for for example 8x10 boards but that seems a bit unneeded.
+}
+//var zoorgt dat de info word gestored zodat het kan worden gebruikt
+//hiermee krijg je het canass geconnect aan dit ding
+var canvas = document.querySelector("canvas");
+var drawingSurface = canvas.getContext("2d");
+//met dit kan de 2d rendering ofzo dus je kan images en dingen doen ja topie
+
+
+//maakt de sprite war je charather is
+var spriteObject =
+{
+    x: 0,
+    y: 0,
+    width: 60,
+    height: 60,
+};
+// de tile map 1= collision en o= loop space
+var tileMap = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,]
+   
+    
+  
+   
+    
+];
+//groote van elke tile in de tile map
+var tileSize = 32;
+//dit kijkt naar de tilemap van col(x) en row(y) en als ze daar zitten dan kijkt hij of het een 1 is als het geen 1 is word het false
+function isSolidTileAtXY(x,y){
+var col = Math.floor(x / tileSize);
+var row = Math.floor(y / tileSize);
+
+if (row >= 0 && row < tileMap.length  && col >0 && col <tileMap[0].length ) {
+    return tileMap[row][col] ===1;
+}
+    return false;
+}
+
+//dit maak de sprite aan/ en de begin positie
+var amphiboleFull = Object.create(spriteObject);
+amphiboleFull.x = 100;
+amphiboleFull.y = 100;
+//is de image voor de sprite
+
+var image = new Image();    
+image.addEventListener("load", loadhandler, false);
+image.src = "media/amphiboleFull.png";
+
+
+
+
+//de veriable de de snelheid en de richtingen
+var xspeed = 0;
+var yspeed = 0;
+var moveLeft = false;
+var moveRight = false;
+var moveUp = false;
+var moveDown = false;
+//event listener voor de arrow keys dus als je een arrow key in indrukt word het true en als je hem lostlaat false
+window.addEventListener("keydown", function (e) {
+    switch (e.key) {
+        case "ArrowUp":
+            moveUp = true;
+            break;
+        case "ArrowDown":
+            moveDown = true;
+            break;
+        case "ArrowLeft":
+            moveLeft = true;
+            break;
+        case "ArrowRight":
+            moveRight = true;
+            break;
+    }
+
+}, false);
+//hetzelfde als de vorige maar nu voor up
+window.addEventListener("keyup", function (e) {
+    switch (e.key) {
+        case "ArrowUp":
+            moveUp = false;
+            break;
+        case "ArrowDown":
+            moveDown = false;
+            break;
+        case "ArrowLeft":
+            moveLeft = false;
+            break;
+        case "ArrowRight":
+            moveRight = false;
+            break;
+    }
+
+}, false);
+//laad de image en als die is geladen blijft hij updaten
+function loadhandler() {
+    update();
+}
+
+
+
+
+
+//zorgt ervoor dan window animatie blijft updaten en cover de rest van de code
+function update() {
+    //dit kijkt naar de sprite en voorspelt waar hij is
+    window.requestAnimationFrame(update, canvas);
+    var futureX = amphiboleFull.x + xspeed;
+    var futureY = amphiboleFull.y + yspeed;
+//dit kijkt voor horizontale movment of hij tegen een collison tile  gaat enzo niet dat update hij
+ if (!isSolidTileAtXY(futureX, amphiboleFull.y) && 
+ !isSolidTileAtXY(futureX + amphiboleFull.width, amphiboleFull.y) && 
+ !isSolidTileAtXY(futureX, amphiboleFull.y + amphiboleFull.height) && 
+ !isSolidTileAtXY(futureX + amphiboleFull.width, amphiboleFull.y + amphiboleFull.height)) {
+ amphiboleFull.x = futureX; 
+}
+
+//hetzelfda maar dan vertical
+if (!isSolidTileAtXY(amphiboleFull.x, futureY) && 
+ !isSolidTileAtXY(amphiboleFull.x + amphiboleFull.width, futureY) && 
+ !isSolidTileAtXY(amphiboleFull.x, futureY + amphiboleFull.height) && 
+ !isSolidTileAtXY(amphiboleFull.x + amphiboleFull.width, futureY + amphiboleFull.height)) {
+ amphiboleFull.y = futureY; 
+}
+//dit is de movement de ! is als die niet is ingerukt
+if (moveUp && !moveDown) {
+        yspeed = -3;
+    }
+    if (moveDown && !moveUp) {
+        yspeed= 3;
+    }
+    if (moveLeft && !moveRight) {
+        xspeed = -3;
+    }
+    if (moveRight && !moveLeft) {
+        xspeed = 3;
+    }
+    if (!moveUp && !moveDown) {
+        yspeed = 0;
+    }
+    if (!moveLeft && !moveRight) {
+        xspeed = 0;
+    }
+    //kijkt of het kleiner is dan nul en als dat zo is zou die moeten stoppen maar ik een met de tilemap een soor van border gemaakt dus dit booit niet echt op dit moment
+    if (amphiboleFull.x < 0) {
+        xspeed = 0;
+    }
+    if (amphiboleFull.y < 0) {
+        yspeed = 0;
+    }
+    if (amphiboleFull.x + amphiboleFull.width > canvas.width) {
+        amphiboleFull.x = canvas.width - amphiboleFull.width;
+    }
+    if (amphiboleFull.y + amphiboleFull.height > canvas.height) {
+        amphiboleFull.y = canvas.height - amphiboleFull.height;
+    }
+    render();
+
+}
+//dit teken de spirte op een nieuwe positie en cleared de canvas elke keer als die op de nieuwe positie komt
+function render() {
+    drawingSurface.clearRect(0, 0, canvas.width, canvas.height);
+    drawingSurface.drawImage(
+        image,
+        Math.floor(amphiboleFull.x), Math.floor(amphiboleFull.y),
+        amphiboleFull.width, amphiboleFull.height
+
+    );
+}
